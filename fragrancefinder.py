64d07a60e6ac4b764203c55b869512fbbae101d8
Landonwkelly <lkelly41@students.kennesaw.edu>
2024-04-24 22:01:36 -0400

    Update fragrancefinder.py
    In this update, I have made two lists separating masculine and feminine scents as this will be the first iteration of the list within my program. I have also added the beginning message and the first question that will be asked by the program

--- fragrancefinder.py
+++ fragrancefinder.py
@@ -1,1 +1,10 @@
-print("Hello")
+fragrances_masculine = ["Sauvage, Acqua Di Gio, Terre de Hermes, Oud Wood, Bleu de Chanel"]
+fragrances_feminine = ["Chanel No 5, Miss Dior, Opium, Portrait of a Lady, Black Orchid "]
+
+print(" Welcome to Fragrance Finder!\nLet's find out a fragrance that matches your style and type by answering a couple questions")
+
+user_input = input("Are you looking for a masculine or feminine smell? M or F to answer: ")
+
+if user_input.capitalize() == "M":
+    user_input_masculine = input("What season would you like this for? Type Spring, Summer, Winter, Fall, or Year-Round")
+
